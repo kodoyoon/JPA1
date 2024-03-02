73540e1af51c3bc429e46d6db762e4f9f1dfbdb0
doyoon <doyoun0527@naver.com>
2024-03-02 15:35:54 +0900

    단방향 연관관계

---
+++ src/main/java/hellojpa/Team.java
@@ -0,0 +1,47 @@
+package hellojpa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Team {
+
+    @Id@GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "TEAM_ID")
+    private Long teamId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setName(String teamA) {
+    }
+}
+
