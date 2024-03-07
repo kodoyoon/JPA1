212a7a6e63f599ab2c6976e5b25a809406a44018
doyoon <doyoun0527@naver.com>
2024-03-07 22:17:41 +0900

    값 타입의 비교

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -3,11 +3,10 @@ package hellojpa;
 public class ValueMain {
     public static void main(String[] args) {
 
-       Integer a = new Integer(10);
-       Integer b = a;
+       int a = 10;
+       int b = 10;
 
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+
     }
 }
