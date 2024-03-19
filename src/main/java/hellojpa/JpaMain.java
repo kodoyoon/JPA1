ab0ce9db3435025894d360c37432361c6be14222
doyoon <doyoun0527@naver.com>
2024-03-19 23:11:24 +0900

    객체지향 쿼리 언어 - 소개

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,44 +21,10 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeaddress(new Address("homecity" ,"street" ,"10000"));
-
-            //단순할때만 컬렉션 사용
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("피자");
-            member.getFavoriteFoods().add("족발");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("========start========");
-
-           Member findMember =  em.find(Member.class,member.getId());
-//home -> new
-// findMember.getHomeaddress().setCity("newCity");
-
-            //통으로 새롭게 해야함 ,
-//            findMember.getHomeaddress().setCity("newCity");
-//            Address a = findMember.getHomeaddress();
-//            findMember.setHomeaddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            //치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-
-            //주소변경(old -> new) , 컬렉션들은 대부분 equals 사용
-//            findMember.getAddressHistory().remove(new AddressEntity("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new AddressEntity("newCity1", "street", "10000"));
-
-
-
+        List<Member>  result = em.createQuery(
+            "select m From Member m where m.username like '%hello%'",
+            Member.class
+                ).getResultList();
 
 
             tx.commit();
