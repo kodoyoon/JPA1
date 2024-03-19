0da0b3ba50b8b0285d204ef922fb5af00315aa8c
doyoon <doyoun0527@naver.com>
2024-03-19 23:40:25 +0900

    객체지향 쿼리 언어 - 소개(네이티브 SQL)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,15 +4,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
-
-import java.util.List;
-import java.util.Set;
-
-
-
 
 public class JpaMain {
 
@@ -31,13 +22,13 @@ public class JpaMain {
 
             //flush -> commit, query
 
-         List<Member> resultList =  em.createNativeQuery("select MEMBER_ID, city, street, zipcode ,USERNAME from MEMBER",Member.class)
-                    .getResultList();
+            em.flush();
+            //결과 0
+            //dbconn.excuteQuery("select * from member");
 
             for (Member member1 : resultList) {
                 System.out.println("member1 = " + member1);
             }
-
 
             tx.commit();
         } catch (Exception e) {
